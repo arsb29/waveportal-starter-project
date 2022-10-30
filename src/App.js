df9a29a92ed40ca7ece573ffffbfabc9e591fcaa
Arseny Belyaev <ars-bel@mail.ru>
2022-10-31 00:06:49 +0300

    Metamask

--- src/App.js
+++ src/App.js
@@ -1,29 +1,73 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, {useEffect, useState} from 'react';
+import { ethers } from 'ethers';
 import './App.css';
 
-export default function App() {
 
-  const wave = () => {
-    
-  }
+const getEthereumObject = () => window.ethereum;
+
+/*
+ * This function returns the first linked account found.
+ * If there is no account linked, it will return null.
+ */
+const findMetaMaskAccount = async () => {
+    try {
+        const ethereum = getEthereumObject();
+
+        /*
+        * First make sure we have access to the Ethereum object.
+        */
+        if (!ethereum) {
+            console.error("Make sure you have Metamask!");
+            return null;
+        }
+
+        console.log("We have the Ethereum object", ethereum);
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+
+        if (accounts.length !== 0) {
+            const account = accounts[0];
+            console.log("Found an authorized account:", account);
+            return account;
+        } else {
+            console.error("No authorized account found");
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+export default function App() {
+    const [currentAccount, setCurrentAccount] = useState('');
+
+    /*
+    * The passed callback function will be run when the page loads.
+    * More technically, when the App component "mounts".
+    */
+    useEffect(async () => {
+        const account = await findMetaMaskAccount();
+        if (account !== null) {
+            setCurrentAccount(account);
+        }
+    }, []);
   
   return (
-    <div className="mainContainer">
+      <div className="mainContainer">
+          <div className="dataContainer">
+              <div className="header">
+                  👋 Hey there!
+              </div>
 
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
+              <div className="bio">
+                  I am Farza and I worked on self-driving cars so that's pretty cool
+                  right? Connect your Ethereum wallet and wave at me!
+              </div>
 
-        <div className="bio">
-        I am Arseny and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
-
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
+              <button className="waveButton" onClick={null}>
+                  Wave at Me
+              </button>
+          </div>
       </div>
-    </div>
   );
 }
